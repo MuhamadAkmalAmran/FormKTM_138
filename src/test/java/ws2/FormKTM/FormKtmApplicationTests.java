333abb938c373fb2d0dd2b5d2f331b9ef62a99b6
akmal <akmal@DESKTOP-QEPVMLM>
2022-11-11 22:01:52 +0700

    First Commit

---
+++ src/test/java/ws2/FormKTM/FormKtmApplicationTests.java
@@ -0,0 +1,13 @@
+package ws2.FormKTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
