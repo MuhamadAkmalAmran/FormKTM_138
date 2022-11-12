5ed7dfbe5380d3b3a974c2e492851eb714c3b8e0
akmal <akmal@DESKTOP-QEPVMLM>
2022-11-12 23:09:32 +0700

    Add title and heading in html

--- src/main/java/ws2/FormKTM/ktmController.java
+++ src/main/java/ws2/FormKTM/ktmController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ktmController {
     
-    @RequestMapping ("")
+    @RequestMapping ("/formktm")
     @ResponseBody
     
     public String formktm(){
